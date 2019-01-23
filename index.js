000376ff2959a13028dbcb1a5a54f5f501a71af5
Malachi Soord <inverse.chi@gmail.com>
2019-01-23 21:42:47 +0100

    Upgrade dependencies, fix indents

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 const scriptUrl = 'https://gitcdn.link/repo/web-scrobbler/web-scrobbler/master/src/core/connectors.js';
 
 require([scriptUrl], (connectors) => {
-    $(() => {
-        let sortedConnectors = connectors.sort((a, b) => {
-            return a.label.localeCompare(b.label);
-        });
-        
-        for (let connector of sortedConnectors) {
-            let item = $('<li/>').text(connector.label);
-            $('#connectors-list').append(item);
-        }
-    })
+	$(() => {
+		let sortedConnectors = connectors.sort((a, b) => {
+			return a.label.localeCompare(b.label);
+		});
+		
+		for (let connector of sortedConnectors) {
+			let item = $('<li/>').text(connector.label);
+			$('#connectors-list').append(item);
+		}
+	})
 });

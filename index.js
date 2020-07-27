a3b4214f32378908d6fc4aa5a991bb3a18adf54c
alexesprit <alex.esprit@gmail.com>
2020-07-27 11:25:11 +0300

    Use CSS to hide all websites

--- index.js
+++ index.js
@@ -3,49 +3,38 @@
 const CONNECTORS_URL =
 	'https://raw.githubusercontent.com/web-scrobbler/website-resources/master/resources/connectors.json';
 
-const websitesFirstId = 'websites-first';
-const websitesMoreId = 'websites-more';
+const websitesContainerId = 'websites';
 const btnShowAllId = 'show-all';
 
 const websitesCountId = 'websites-count';
 
 function onDomContentLoaded() {
 	initControls();
-	updateWebsites();
 }
 
-function initControls() {
+async function initControls() {
+	const connectors = (await getConnectors()).sort(compareIgnoreCase);
+
+	const websitesContainer = document.getElementById(websitesContainerId);
+
 	const btnShowAll = document.getElementById(btnShowAllId);
 	btnShowAll.addEventListener('click', (e) => {
 		e.preventDefault();
 
-		document.getElementById(websitesMoreId).hidden = false;
+		websitesContainer.classList.add('websites--more');
 		btnShowAll.hidden = true;
 	});
-}
-
-async function updateWebsites() {
-	const connectors = (await getConnectors()).sort(compareIgnoreCase);
 
 	const websitesCount = document.getElementById(websitesCountId);
 	websitesCount.innerHTML = connectors.length;
 
-
-	const initialWebsitesCount = getInitialWebsitesCount();
-	appendConnectors(websitesFirstId, connectors.slice(0, initialWebsitesCount));
-	appendConnectors(websitesMoreId, connectors.slice(initialWebsitesCount));
-}
-
-function appendConnectors(containerId, websites) {
-	const container = document.getElementById(containerId);
-
-	for (const websiteName of websites) {
+	for (const websiteName of connectors) {
 		const websiteLabel = document.createTextNode(websiteName);
 		const websiteElement = document.createElement('li');
 		websiteElement.className = 'websites__item';
 		websiteElement.appendChild(websiteLabel);
 
-		container.appendChild(websiteElement);
+		websitesContainer.appendChild(websiteElement);
 	}
 }
 
@@ -54,10 +43,6 @@ async function getConnectors() {
 	const connectors = await response.json();
 
 	return connectors;
-}
-
-function getInitialWebsitesCount() {
-	return document.body.clientWidth >= 768 ? 36 : 18;
 }
 
 function compareIgnoreCase(a, b) {

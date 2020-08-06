64dce7cc7b498dc05f30683ed419dbd3e6eb90ab
alexesprit <alex.esprit@gmail.com>
2020-08-07 01:11:18 +0300

    Ignore changes in `.github` and `node_modules` dirs

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 'use strict';
 
+const ignoredDirs = ['.github', 'node_modules'];
+
+function getIgnoredPaths() {
+	return ignoredDirs.map((dir) => `**/${dir}/**`);
+}
+
 require('live-server').start({
 	host: '127.0.0.1',
 	file: 'index.html',
+	ignore: getIgnoredPaths(),
 	wait: 1000,
 	logLevel: 2,
 });

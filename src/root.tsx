5bce47adfec0b742b1eefe48f64a40f915314fec
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2023-05-25 15:06:52 +0800

    Remove references to wss

--- src/root.tsx
+++ src/root.tsx
@@ -42,21 +42,21 @@ export default function Root() {
 				<Link
 					rel="apple-touch-icon"
 					sizes="180x180"
-					href="/wss/apple-touch-icon.png"
+					href="/apple-touch-icon.png"
 				/>
 				<Link
 					rel="icon"
 					type="image/png"
 					sizes="32x32"
-					href="/wss/favicon-32x32.png"
+					href="/favicon-32x32.png"
 				/>
 				<Link
 					rel="icon"
 					type="image/png"
 					sizes="16x16"
-					href="/wss/favicon-16x16.png"
+					href="/favicon-16x16.png"
 				/>
-				<Link rel="manifest" href="/wss/site.webmanifest" />
+				<Link rel="manifest" href="/site.webmanifest" />
 			</Head>
 			<Body>
 				<Suspense>
